ee2a760bc096a6c832ef942da6e3af327104632f
ayodyabanuka <ayodyabanukafernando@gmail.com>
2025-04-12 15:08:16 +0530

    favicon update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
   return (
     <html lang='en'>
       <head>
-        <link rel='shortcut icon' href='/favicon.webp' type='image/x-icon' />
+        <link rel='shortcut icon' href='/favicon.png' type='image/x-icon' />
       </head>
       <body className={`${sinhala.className}  antialiased`}>{children}</body>
     </html>
